d4f20450246db0d3575e91e9c765809e826a73aa
alibaseit <alibase@gmail.com>
2017-03-11 01:39:00 +0300

    tabs replaced with spaces

--- src/main/java/com/miaweb/config/WebConfiguration.java
+++ src/main/java/com/miaweb/config/WebConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @Configuration
-
 @EnableSpringDataWebSupport
 public class WebConfiguration {
 	@Bean
