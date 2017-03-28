f882638082ce89994a31d1daacd40f76ecf08927
alibaseit <alibase@gmail.com>
2017-03-29 01:21:38 +0300

    code cleaned

--- src/main/java/com/miaweb/config/SwaggerConfig.java
+++ src/main/java/com/miaweb/config/SwaggerConfig.java
@@ -13,20 +13,25 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2 // Enable swagger 2.0 spec
 public class SwaggerConfig {
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.miaweb.controller")).paths(PathSelectors.any()).build()
-				.apiInfo(apiInfo());
-	}
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.miaweb.controller"))
+                .paths(PathSelectors.any()).build()
+                .apiInfo(apiInfo());
+    }
 
-	private ApiInfo apiInfo() {
-		ApiInfo apiInfo = new ApiInfo("My REST API", "Some custom description of API.", "API TOS", "Terms of service",
-				contact("Ali Dogan", "alibaseit.com", "myeaddress@company.com"), "License of API", "API license URL");
-		return apiInfo;
-	}
+    private ApiInfo apiInfo() {
+        ApiInfo apiInfo = new ApiInfo("My REST API",
+                "Some custom description of API.",
+                "API TOS",
+                "Terms of service",
+                contact("Ali Dogan", "alibaseit.com", "myeaddress@company.com"),
+                "License of API", "API license URL");
+        return apiInfo;
+    }
 
-	private Contact contact(String name, String url, String email) {
-		return new Contact(name, url, email);
-	}
+    private Contact contact(String name, String url, String email) {
+        return new Contact(name, url, email);
+    }
 }
