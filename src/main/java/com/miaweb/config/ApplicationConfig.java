fd221be2c64d52971c818d836143b48591b51214
alibaseit <alibase@gmail.com>
2017-05-16 09:42:45 +0300

    scheduled task added

--- src/main/java/com/miaweb/config/ApplicationConfig.java
+++ src/main/java/com/miaweb/config/ApplicationConfig.java
@@ -3,12 +3,14 @@ package com.miaweb.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableJpaRepositories(basePackages="com.miaweb.repository.jpa")
 @EnableAspectJAutoProxy
-@EnableTransactionManagement	
+@EnableTransactionManagement
+@EnableScheduling
 public class ApplicationConfig {
 
 }
